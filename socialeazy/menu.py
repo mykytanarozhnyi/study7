dff00713e2421db68a2bedd54897504b790d4498
Никита Нарожный <nikitanaroznyj@MacBook-Air-Nikita.local>
2024-04-24 17:05:58 +0200

    new changes

--- socialeazy/menu.py
+++ socialeazy/menu.py
@@ -1,25 +1,46 @@
 from post import Post
-if __name__ == "__main__":
-    posts = list()
-    while True:
-        print("Welcome to the eazy")
-        message = ("""Choose the option:
-        1. Add post
-        2. See all posts 
-        0. exit")
-        
-        Your Choice: """)
 
+if __name__ == "__main__":
+    while True:
+        print("Welcome to the new social")
+        message = ("""
+            Choose the option:
+            1. Add post
+            2. See all posts
+            3. Like a post
+            4. Dislike a post
+            5. Ratings posts
+            0. Exit 
+
+            Your Choice: """)
         choice = input(message)
         match choice:
-            case "1" | "155":
+            case "1":
                 new_post = Post()
-                posts.append(new_post)
             case "2":
                 for entry in Post.entries:
                     print(entry)
+            case "3":
+                post_id = input("Enter the ID post to like: ")
+                for entry in Post.entries:
+                    if entry.id == int(post_id):
+                        entry.like()
+                        print(entry.likes)
+                        break
+            case "4":
+                post_id = input("Enter the ID post to dislike: ")
+                for entry in Post.entries:
+                    if entry.id == int(post_id):
+                        entry.dislike()
+                        print(entry.dislikes)
+                        break
+            case "5":
+                post_id = input("Enter the ID post to see rating:")
+                for entry in Post.entries:
+                    if entry.id == int(post_id):
+                        print(entry.rating())
+                        break
             case "0":
                 break
             case _:
-                print("try something else")
-
+                print("Wrong choice")
--- socialeazy/post.py
+++ socialeazy/post.py
@@ -1,13 +1,53 @@
 from datetime import datetime
-class Post:
 
-    entries = list()
+
+class Content:
+
     def __init__(self):
         self.author = input("Enter nickname: ")
-        self.text = input("Write something here: ")
+        self.text = input("Write your post: ")
         self.created_at = datetime.now()
-        self.entries.append(self)
-        self.id = len(self.entries)
 
     def __str__(self):
-        return f"#{self.id} {self.author} said: {self.text}"
+        return f"{self.author} said at {self.created_at}: {self.text}"
+
+
+class Post(Content):
+    entries = list()
+
+    def __init__(self):
+        super().__init__()  # Content.__init__()
+        self.entries.append(self)
+        self.id = len(self.entries)
+        self.likes = 0
+        self.dislikes = 0
+
+    def like(self):
+        self.likes += 1
+
+    def dislike(self):
+        self.dislikes += 1
+
+    def rating(self):
+        return self.likes - self.dislikes
+
+    def __str__(self):
+        return f"#{self.id} {self.author} said: {self.text}Likes: {self.likes} | Dislikes: {self.dislikes}"
+
+
+class Image:
+    pass
+
+
+class PostWithImage(Post, Image):
+    pass
+
+
+class Comment(Content):
+
+    def __init__(self, post_id):
+        super().__init__()
+        self.post_id = post_id
+
+    def __str__(self):
+        return f"{self.author} commented on {self.post_id}: {self.text}"
