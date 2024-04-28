2efbc9083153cba5732d12dc7154602cb0753e41
Никита Нарожный <nikitanaroznyj@MacBook-Air-Nikita.local>
2024-04-28 13:59:36 +0200

    fib,gen,iterators, Airbnb readygit add -A

--- Airbnb.py
+++ Airbnb.py
@@ -1,12 +1,59 @@
 class Rental:
-    def __init__(self,name,meters,price):
+    flats = list()
+
+    def __init__(self, name, meters, price, date):
         self.name = name
         self.meters = meters
         self.price = price
-    def print(self):
-        print(f"This is {self.name} has {self.meters} meters, and costs {self.price}")
-if __name__ == "__main__":
-    flat = Rental("Amazing flat","58","1000$")
+        self.date = date
+        Rental.flats.append(self)
 
-print (Rental)
-flat.print()
+    def __str__(self):  # Перевизначаємо метод __str__ для виведення опису помешкання
+        return f"This is {self.name} has {self.meters} meters, and costs {self.price}, available from {self.date}"
+
+class Booking:
+    bookings = list()
+
+    def __init__(self, name, start_date, end_date, rental):
+        self.name = name
+        self.start_date = start_date
+        self.end_date = end_date
+        self.rental = rental
+        self.bookings.append(self)
+
+    @classmethod
+    def book(cls, name, start_date, end_date, rental):
+        for booking in cls.bookings:
+            if booking.rental == rental:
+                if (booking.end_date < start_date or booking.start_date > end_date):
+                    continue
+                else:
+                    print("Rental is already booked")
+                    return
+        cls(name, start_date, end_date, rental)
+        print("Booking successful!")
+
+    @staticmethod
+    def check_availability(start_date, end_date, rental):
+        for booking in Booking.bookings:
+            if booking.rental == rental:
+                if (booking.end_date < start_date or booking.start_date > end_date):
+                    continue
+                else:
+                    return False
+        return True
+
+if __name__ == "__main__":
+    flat1 = Rental("Amazing flat", "58", "160$", "2024-4-20")
+    flat2 = Rental("Dworzec Centralny flat", "25", "110$", "2024-4-27")
+
+    # Виводимо інформацію про всі доступні помешкання
+    print("\nAvailable flats:")
+    for flat in Rental.flats:
+        print(flat)
+
+    # Бронюємо помешкання
+    Booking.book("John", "2024-4-21", "2024-4-25", flat1)
+    Booking.book("Alice", "2024-4-24", "2024-4-30", flat2)
+
+
---
+++ iterators.py
@@ -0,0 +1,41 @@
+from collections.abc import Iterable
+
+class SequenceIterator:
+
+    def __init__(self, sequence):
+        if isinstance(sequence, Iterable):
+            self.sequence = sequence
+        else:
+            self.sequence = list()
+
+    def __iter__(self):
+        self._index = 0
+        return self
+
+    def __next__(self):
+        if self._index < len(self.sequence):
+            element = self.sequence[self._index]
+            self._index += 1
+            return element
+        else:
+            raise StopIteration()
+
+
+class EvenNumberIterator:
+    def __init__(self,limit=100):
+        self.value = 0
+        self.limit = limit
+    def __iter__(self):
+        return self
+    def __next__(self):
+        if self.value < self.limit:
+         self.value += 2
+         return self.value
+        else:
+            raise StopIteration()
+
+if __name__ == "__main__":
+    for element in SequenceIterator([1, 2, 3]).sequence:
+        print(element)
+    for element in EvenNumberIterator():
+        print(element)
