c12b4cb778fef45977edc5803cbea896a86fc766
mykytanarozhnyi <mofthetruth@gmail.com>
2024-05-20 23:44:43 +0200

    evclide.py updated with timer and decorators

--- alghorythm_complexity.py
+++ alghorythm_complexity.py
@@ -1,25 +1,51 @@
-#0(1)
+from math import gcd
+# O(1)
 def return_first(iterable):
     return iterable[0]
-#0 (log(n))
+
+# O(log(n))
 def binary_search(iterable):
     pass
-#0(n)
+
+# O(n**k, k<1)
+def tree_search(tree):
+    pass
+
+# O(n)
 def double_value(iterable):
     result = list()
-    for element in iterable:
-        result.append(element * 2 )
+    for element in interable:
+        result.append(element * 2)
     return result
-    #return [element * 2 for element in iterable]
+    # return [element *2 for element in iterable]
+
+# O(n*log(n))
+def find_max():
+    the_list = [[1, 2, 3], [9, 6, 4, 45, 7], [4, 7, 8, 11]]
+    tmp_max = 0
+    for sublist in the_list:
+        for element in sublist:
+            if element > tmp_max:
+                tmp_max = element
+
+    sublist_max = []
+    for sublist in the_list:
+        sublist_max.append(max(sublist))
+    return max(sublist_max)
+
+# O(2 * n) => O(n)
 def print_double(iterable):
     result = list()
-    for element in iterable:
+    for element in interable:
         result.append(element * 2)
-        for item in result:
-            print(result)
-#0(n**2)
+    for item in result:
+        print(item)
+
+# O(n**2)
 def my_sort(iterable):
     for element in iterable:
         for other_element in iterable:
             if element < other_element:
-                #swap elements
+                # swap elements
+
+# O(e**n)
--- evclide.py
+++ evclide.py
@@ -1,18 +1,56 @@
-def gcd_classic(m,n):
+import math
+import time
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+def log_decorator(debug=True):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+        # запам'ятовує час початку виконання задекорованої функції
+            start_time = time.time()
+            if debug:
+                logger.info(f"Execution started at {start_time}")
+            result = func(*args, **kwargs)
+            end_time = time.time()
+            if debug:
+                logger.info(f"Execution ended at {end_time}")
+            logger.info(f"Execution took {end_time - start_time}")
+            return result
+        return wrapper
+    return decorator
+
+@log_decorator(debug=True)
+def gcd_classic(m, n):
     while m != n:
         if m > n:
             m = m - n
         else:
             n = n - m
-            return m
-def gcd_optimised(a,b):
+    return m
+
+@log_decorator(debug=True)
+def gcd_optimised(a, b):
     r = a % b
     while r:
         a = b
         b = r
         r = a % b
+    return b
+
+@log_decorator(debug=True)
+def math_gcd(a,b):
+    math.gcd(a,b)
+    result = math.gcd(a,b)
+    return result
+
+
+
 
 if __name__ == "__main__":
-    gcd_classic(100,1)
-    print ("============")
-    gcd_optimised(27,6)
+    print(gcd_classic(999_999, 2))
+    print("=============")
+    print(gcd_optimised(999_999, 2))
+    print(math_gcd(999_999,2))
+
