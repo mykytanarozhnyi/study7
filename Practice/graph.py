166de10d70840511b868663c7573e5228dcd2b5e
mykytanarozhnyi <mofthetruth@gmail.com>
2024-06-13 19:54:51 +0200

    graph.py added

--- Practice/graph.py
+++ Practice/graph.py
@@ -1,12 +1,12 @@
 class Graph:
+
     def __init__(self):
         self.vertexes = {}
 
-    def add_vertex(self,name):
+    def add_vertex(self, name):
         if name in self.vertexes:
             return
-        else:
-            self.vertexes.update({name: set()})
+        self.vertexes.update({name: set()})
 
     def add_edge(self, from_name, to_name):
         self.add_vertex(from_name)
@@ -15,28 +15,49 @@ class Graph:
         self.vertexes[to_name].add(from_name)
 
     def print_vertexes(self):
-        for vertex, neighbours in self.vertexes.items():
-            print(f"{vertex}: {neighbours}")
+        for vertex, neighbors in self.vertexes.items():
+            print(f"{vertex}: {neighbors}")
 
-    # ДЗ def find_loops(self):
-        pass
+    def find_loops(self):
+        loops = []
+        for vertex,neighbors in self.graph.items():
+            if vertex in neighbors:
+                loops.append(vertex)
+        return loops
 
-    # ДЗ def find_isolated(self):
-        pass
-
+    def find_isolated(self):
+        isolated_vertices = []
+        for vertex in self.graph:
+            if not self.graph[vertex]:
+                isolated_vertices.append(vertex)
+            else:
+                is_isolated = True
+                for neighbors in self.graph.values():
+                    is_isolated = False
+                    break
+                if is_isolated:
+                    isolated_vertices.append(vertex)
+        return isolated_vertices
 if __name__ == "__main__":
     graph = Graph()
-    graph.add_edge("a","a")
-    graph.add_edge("a", "b")
     graph.add_edge("a", "a")
+    graph.add_edge("a", "b")
+    graph.add_edge("b", "a")
+    graph.add_edge("b", "d")
+    graph.add_edge("b", "e")
+    graph.add_vertex("c")
+    graph.add_edge("d", "b")
+    graph.add_edge("d", "e")
+    graph.add_edge("e", "b")
+    graph.add_edge("e", "d")
     graph.print_vertexes()
 
 """
 {
-    "a": {,"a", "b"},
-    "b": {"a", "d", "e"}
+    "a": {"a", "b"},
+    "b": {"a", "d", "e"},
     "c": {},
     "d": {"b", "e"},
     "e": {"b", "d"},
-    }
+}
 """
